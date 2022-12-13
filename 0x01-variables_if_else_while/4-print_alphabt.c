ba62e4c66e56de8861e861b577904f254fca6127
Ulalo Banda <ulalobanda@gmail.com>
2022-12-12 16:53:13 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - print alphabt
+ *
+ *
+ *return: 0
+ */
+
+int main(void){
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++){
+		if(ch == 'e' || ch == 'q')
+			continue;
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+return(0);
+}
