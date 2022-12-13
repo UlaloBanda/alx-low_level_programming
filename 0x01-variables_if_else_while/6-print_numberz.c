aef5e050c6de4747888c62406413cb4022f8f986
Ulalo Banda <ulalobanda@gmail.com>
2022-12-12 17:33:30 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print numberz
+ *
+ * return:0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+return(0);
+}
