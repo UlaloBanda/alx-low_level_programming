874ed78b7027a77fce25186979e4a1897e74b43f
Ulalo Banda <ulalobanda@gmail.com>
2022-12-12 17:18:21 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print numbers
+ *
+ *
+ *return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+return(0);
+}
