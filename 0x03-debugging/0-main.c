f6882da84e8803bb8eb18d93315c6fc57cc3e9cb
Ulalo Banda <ulalobanda@gmail.com>
2022-12-16 13:04:07 -0800

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - tests function print if int is positive or negative
+ *
+ *
+ * Return : 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,23 @@
+C - debugging
+
+This project involves debugging with ducks!!
+
+Tasks 
+
+0. Multiple mains
+
+
+1. Like, comment, subscribe
+
+
+
+
+2. 0 > 972?
+
+
+3. Leap year
+
+
+
+
+
